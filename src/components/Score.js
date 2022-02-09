96d36788dfc94463522898a37e5a30f2cf4cf7b4
Nicky-09 <nikitasharma2999@gmail.com>
2022-02-10 02:27:18 +0530

    quiz app base

---
+++ src/components/Score.js
@@ -0,0 +1,49 @@
+import Highcharts from "highcharts/highstock";
+import PieChart from "highcharts-react-official";
+
+
+function Score({ score, timer, withTimer,totalQuestions,gameEnded }) {
+
+
+    const options = {
+  chart: {
+    type: "pie"
+  },
+  series: [
+    {
+      data: [
+        {
+            name: "Total Questions",
+          y: totalQuestions*100
+        },
+        {
+            name: "Correct Answers",
+          y: score
+        }
+      ],
+      colors: ['red', 'green'],
+    }
+   
+  ]
+};
+
+    console.log(totalQuestions)
+	return (
+		<div className='score-timer-container'>
+			{withTimer && (
+				<div className='score-timer-border'>
+					<div className='score-timer-text'>Timer</div>
+					<div className='score-timer-number'>{timer}</div>
+				</div>
+			)}
+			<div>
+				<div className='score-timer-text'>Score</div>
+				<div className='score-timer-number'>{score}/{totalQuestions}00</div>
+			</div>
+            {gameEnded? <PieChart highcharts={Highcharts} options={options} />:''}
+           
+		</div>
+	)
+}
+
+export default Score
